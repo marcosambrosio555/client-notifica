dc66f85d15eff2c9ecaa92587d78f346d5ad685e
Marcos Ambrósio <marcosambrosio555@gmail.com>
2025-02-15 09:48:53 +0100

    variable edited

--- src/components/ModalSendMessage.jsx
+++ src/components/ModalSendMessage.jsx
@@ -19,9 +19,12 @@ export function ModalSendMessage({ usersSelected, message, setModalSendMessage }
         }
         return `${users[0].username}, ${users[1].username} e mais ${users.length - 2} pessoas`
     }
-
+ 
     async function handleSend(e) {
+        
         consol.log("Ok")
+        console.log(`${apikey}message`)
+
         await fetch(`${apikey}message`, {
             method: "POST",
             headers: {
